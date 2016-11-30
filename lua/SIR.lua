68d330076b72104d8f46d3d815a8de5668e147a1
Pedro Andrade <pedro.andrade@inpe.br>
2016-11-30 12:39:45 -0200

    SIR updated to support calibration needs.

--- lua/SIR.lua
+++ lua/SIR.lua
@@ -4,19 +4,23 @@
 -- @arg data.infected The initial indected population. The default value is 2.
 -- @arg data.recovered The initial recovered population. The default value is 0.
 -- @arg data.duration The duration of the disease in days. The default value is 2.
+-- @arg data.contacts The number of contacts each infected has each time step. The default value is 6.
 -- @arg data.finalTime The final simulation time, in days. The default value is 30.
 -- @arg data.probability The probability of a susceptible person getting infected after
--- a contact with an infected one.
+-- a contact with an infected one. The default value is 0.25.
 -- @arg data.maximum A threshold of infected people that activates a public policy. The
 -- policy asks people to start leaving their houses, which cuts the contacts
 -- between people by half.
 -- @image sir.bmp
+-- @output maxInfected The maximum number of infected individuals along the simulation.
+-- @output finalInfected A table with number of infected individuals in each simulation step.
 SIR = Model{
 	susceptible = 9998,
 	infected = 2,
 	recovered = 0,
 	duration = 2,
 	finalTime = 30,
+	contacts = 6,
 	maximum = math.huge,
 	probability = 0.25,
 	init = function(model)
@@ -26,20 +30,27 @@ SIR = Model{
 			color = {"green", "red", "blue"}
 		}
 	
-		model.contacts = 6
+		model.maxInfected = model.infected
+		model.finalInfected = {model.infected}
 	
 		model.timer = Timer{
 			Event{action = function()
-				 local proportion = model.susceptible / 
-					 (model.susceptible + model.infected + model.recovered)
+				local proportion = model.susceptible / 
+					(model.susceptible + model.infected + model.recovered)
 				
-				 local newInfected = model.infected * model.contacts * model.probability * proportion
+				local newInfected = model.infected * model.contacts * model.probability * proportion
 	
-				 local newRecovered = model.infected / model.duration
+				local newRecovered = model.infected / model.duration
 				 
-				 model.susceptible = model.susceptible - newInfected
-				 model.recovered = model.recovered + newRecovered
-				 model.infected = model.infected + newInfected - newRecovered
+				model.susceptible = model.susceptible - newInfected
+				model.recovered = model.recovered + newRecovered
+				model.infected = model.infected + newInfected - newRecovered
+
+				if model.maxInfected < model.infected then
+					model.maxInfected = model.infected
+				end
+
+				table.insert(model.finalInfected, model.infected)
 			end},
 			Event{action = function()
 				if model.infected >= model.maximum then
