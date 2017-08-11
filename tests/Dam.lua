1a789c23a9737c985abf8d44020481c853169f04
Rainer <rainerjms@gmail.com>
2017-08-11 16:19:46 -0300

    #16 Update Model e Teste

--- tests/Dam.lua
+++ tests/Dam.lua
@@ -8,13 +8,11 @@ return{
 		model:run()
 
 		unitTest:assertSnapshot(model.chart, "Dam-chart-1.bmp", 0.15)
-		
 		model = Dam{countYear = true}
 
 		model:run()
 
 		unitTest:assertSnapshot(model.chart, "Dam-chart-2.bmp", 0.12)
-		
 	end,
 }
 
