f7eeb969051f4c23c93cc7ac5a37a8f05da3ba60
Pedro Andrade <pedro.andrade@inpe.br>
2017-08-11 15:30:27 -0300

    Close #16. Adding Dam model.

---
+++ tests/Dam.lua
@@ -0,0 +1,19 @@
+-- Test file for Dam.lua
+-- Author: Gilberto Camara and Pedro R. Andrade
+
+return{
+	Dam = function(unitTest)
+		local model = Dam{}
+
+		model:run()
+
+		unitTest:assertSnapshot(model.chart, "Dam-chart-1.bmp")
+
+		model = Dam{countYear = true}
+
+		model:run()
+
+		unitTest:assertSnapshot(model.chart, "Dam-chart-2.bmp")
+	end,
+}
+
