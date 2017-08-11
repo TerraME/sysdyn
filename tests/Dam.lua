3602095935330c2e2c976fd84238e6e3b0bac10d
SKYVENGER\Fabricio <fabriciorodriguespereira@yahoo.com.br>
2017-08-11 16:10:16 -0300

    #16 update Dam Model

--- tests/Dam.lua
+++ tests/Dam.lua
@@ -10,7 +10,7 @@ return{
 		unitTest:assertSnapshot(model.chart, "Dam-chart-1.bmp", 0.15)
 
 		model = Dam{countYear = true}
-		
+
 		model:run()
 
 		unitTest:assertSnapshot(model.chart, "Dam-chart-2.bmp", 0.12)
